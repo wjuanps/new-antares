6f2144bfcb22247a33bf160cbc211f83caab86b9
Juan Soares <wjuan.ps@gmail.com>
2019-12-28 11:32:46 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'name'       => 'teste',
+        'username'   => 'root',
+        'password'   => '',
+        'connection' => 'mysql:host=localhost',
+        'options'    => []
+    ]
+];
