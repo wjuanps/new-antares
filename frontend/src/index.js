d07e382503efe9cfeab7a879dd8372e18f4a3266
Juan Soares <wjuan.ps@gmail.com>
2020-04-11 20:12:09 -0300

    add assets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
+import "./assets/simple-line-icons/css/simple-line-icons.css";
+import "./assets/css/landing-page.min.css";
+import "./assets/css/styles.css";
+
 import App from "./App";
 
 ReactDOM.render(
